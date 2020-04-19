b39b75686ba243c9327c9e63ca7304f306734b4d
Tom Hermans <tomhermans@gmail.com>
2020-04-19 13:28:38 +0200

    fixed duplicate scripts issue + added first divider component

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,9 @@
 // JS
+console.log("scripts");
+
+let allLinks = document.querySelectorAll("a");
+var allDividers = document.querySelectorAll("divider");
+console.log(allLinks);
 
 fetch("./components/header.html")
   .then((response) => {
@@ -14,4 +19,22 @@ fetch("./components/footer.html")
   })
   .then((data) => {
     document.querySelector("footer").innerHTML = data;
+  });
+
+fetch("./components/container1.html")
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.querySelector(".somediv").innerHTML = data;
+  });
+
+fetch("./components/divider.html")
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    allDividers.forEach((element) => {
+      element.innerHTML = data;
+    });
   });
